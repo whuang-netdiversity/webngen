a257e436a2bdbd5a56d29b4ad86a7f3ca72969fa
Wes Huang <ops@netdiversity.ca>
2025-08-12 10:41:17 -0600

    Config

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ module.exports = {
                 { from: 'css', to: 'css' },
                 { from: 'data', to: 'data' },
                 { from: 'ads.txt', to: 'ads.txt' },
-                { from: 'static/_redirects', to: '_redirects', noErrorOnMissing: true }
+                { from: 'static/_redirects', to: '_redirects', toType: 'file', noErrorOnMissing: true }
             ]
         })
     ],
